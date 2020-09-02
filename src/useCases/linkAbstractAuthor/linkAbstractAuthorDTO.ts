c72face9087ee6e87e1e2b99464ad11782dcc1c8
unknown <keler.yohan@hotmail.com>
2020-09-02 03:31:02 -0300

    correção

--- src/useCases/linkAbstractAuthor/linkAbstractAuthorDTO.ts
+++
@@ -1,7 +0,0 @@
-import { Author } from "../../entities/Author";
-import { Abstract } from "../../entities/Abstract";
-
-export interface linkAbstractAuthorDTO{
-    author: Author,
-    abstract: Abstract
-}
