2b647210aba57a5da8cf1e27fb79affeb638b79d
unknown <keler.yohan@hotmail.com>
2020-09-04 02:52:48 -0300

    verificação se o nome da instituição é único

--- src/repositories/implementations/SQLiteInstitutionsRepository.ts
+++ src/repositories/implementations/SQLiteInstitutionsRepository.ts
@@ -3,11 +3,16 @@ import { IInstitutionsRepository } from '../IInstitutionsRepository';
 import { Institution } from '../../entities/Instituition';
 
 export class SQLiteInstitutionsRepository implements IInstitutionsRepository {
-  async save(institution: Institution): Promise<void> {
-    db('user_institution').insert({
-      "name": institution.name,
-      "abbreviation": institution.abbreviation
-    }).then(function(){});
+    async save(institution: Institution): Promise<void> {
+      const allInstitutions:Institution[] = await this.getAllInstitutions(); 
+      const newInstitutionCheck = (allInstitutions.find(each_institution => each_institution.name.toLocaleUpperCase() == institution.name.toLocaleUpperCase()))
+      //console.log(newInstitutionCheck)
+      if( newInstitutionCheck == undefined){
+        db('user_institution').insert({
+          "name": institution.name,
+          "abbreviation": institution.abbreviation
+      }).then(function(){});
+    }
   }
 
   async getAllInstitutions(): Promise<Institution[]>{  
